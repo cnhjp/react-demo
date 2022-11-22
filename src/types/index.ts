76b071bca3a3a302634e3cd2fb4c7a6407727414
cnhjp <1304272317@qq.com>
2022-11-22 22:42:59 +0800

    🔧 build(路由类型):

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,12 +1,12 @@
 import type { RouteObject } from "react-router-dom";
 
-export type RouteItem = {
+export type RouteItem = RouteObject & {
   meta?: {
     auth?: boolean;
     title?: string;
   };
   children?: RouteItem[];
-} & RouteObject;
+};
 
 export interface NavMenuItem {
   itemKey: string;
